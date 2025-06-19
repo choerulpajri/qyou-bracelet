54ed5ddcd320ed9e277c62367ac2444ca1247508
choerulpajri <mrirjaf@gmail.com>
2025-06-20 05:33:38 +0700

    Delete src/pages/app/testqr directory

--- src/pages/app/testqr/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestQR from '../../components/TestQR';
-
-export default function TestQRPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <TestQR />
-    </main>
-  );
-}
